eb234dd33481ab0a92c527baba311b75f491fa0d
xirikm <xirikm@gmail.com>
2020-02-06 22:22:51 +0800

    update test.py

--- test.py
+++ test.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
         drawed_img = draw_bboxes(example_img, bboxes, landmarks)
         drawed_img.save("./images/example" + str(i + 1) + "_result.jpg")
 
+    # crop face images
     example_img = Image.open("./images/example3.jpg")
     bboxes, _ = detect_faces(example_img)
-    face_img_list = crop_img(example_img, bboxes, (64, 64))
+    face_img_list = crop_img(example_img, bboxes, size=(64, 64))
     for i in range(len(face_img_list)):
         face_img_list[i].save("./images/example3_croped/" + str(i + 1) + ".jpg")
